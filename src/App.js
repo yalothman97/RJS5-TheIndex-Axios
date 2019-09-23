061f2adbd19b06bfe28219777f47a78a09f07746
Yousef Al Othman <Yousef@MacBook-Pro-2.local>
2019-09-23 12:30:10 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+import Loading from "./Loading";
 
 class App extends Component {
   state = {
     currentAuthor: null,
-    filteredAuthors: authors
+    filteredAuthors: [],
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  selectAuthor = async author => {
+    const response = await axios.get(
+      "https://the-index-api.herokuapp.com/api/authors/" + author.id
+    );
+    const currentAuthor = response.data;
+    this.setState({
+      currentAuthor: currentAuthor
+    });
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -30,16 +40,35 @@ class App extends Component {
   getContentView = () => {
     if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
-    } else {
+    } else if (this.state.loading === true) {
+      return <Loading />;
+    } else if (this.state.loading === false) {
       return (
         <AuthorsList
-          authors={this.state.filteredAuthors}
+          authors={this.state.authors}
           selectAuthor={this.selectAuthor}
           filterAuthors={this.filterAuthors}
         />
       );
     }
   };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors"
+      );
+      console.log(response);
+      const authors = response.data;
+      this.setState({
+        authors: authors,
+        filteredAuthors: authors,
+        loading: false
+      });
+    } catch (error) {
+      console.error("Error, data not fetched.");
+    }
+  }
 
   render() {
     return (
